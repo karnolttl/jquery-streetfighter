3b639f9fefeb8a096237e6eea0f1a10520fceec6
karnolttl <tlkarnolt@gmail.com>
2016-07-11 22:57:42 -0400

    Extension task

--- js/app.js
+++ js/app.js
@@ -1,62 +1,68 @@
+var isPosing = false;
+
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-    $('.ryu-posing').hide();
+  $('.ryu, .hulk-ryu').mouseenter(function() {
+    if (!isPosing) {
+      $('.ryu-still, .hulk-ryu-still').hide();
+      $('.ryu-ready, .hulk-ryu-ready').show();
+    }
   })
   .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-    $('.ryu-throwing').hide();
-    $('.ryu-posing').hide();
+    if (!isPosing) {
+      $('.ryu-ready, .hulk-ryu-ready').hide();
+      $('.ryu-still, .hulk-ryu-still').show();
+      $('.ryu-throwing, .hulk-ryu-throwing').hide();
+    }
   })
   .mousedown(function() {
-    playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
-      {'left': '1020px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css ('left', '520px');
-      });
+    if (!isPosing) {
+      $('#hadouken-sound')[0].volume = 1;
+      $('#hadouken-sound')[0].load();
+      $('#hadouken-sound')[0].play();
+      $('.ryu-ready, .hulk-ryu-ready').hide();
+      $('.ryu-throwing, .hulk-ryu-throwing').show();
+      $('.hadouken').finish().show().animate(
+        {'left': '700px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css ('left', '520px');
+        });
+      $('.hulk-hadouken').finish().show().animate(
+        {'left': '850px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css ('left', '1200px');
+        });
+    }
   })
   .mouseup(function() {
-    $('.ryu-ready').show();
-    $('.ryu-throwing').hide();
+    if (!isPosing) {
+      $('.ryu-ready, .hulk-ryu-ready').show();
+      $('.ryu-throwing, .hulk-ryu-throwing').hide();
+    }
   });
 });
 
+
 $(document).keydown(function(e){
-  if (e.which == 88 || e.which == 120) {
-    playMusic();
-    $('.ryu-posing').show();
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').hide();
-    $('.ryu-still').hide();
+  if (e.which == 88) {
+    isPosing = true;
+    $('#intro-music')[0].volume = 1;
+    $('#intro-music')[0].play();
+    $('.ryu-posing, .hulk-ryu-posing').show();
+    $('.ryu-throwing, .hulk-ryu-throwing').hide();
+    $('.ryu-ready, .hulk-ryu-ready').hide();
+    $('.ryu-still, .hulk-ryu-still').hide();
   }
 })
   .keyup(function(e){
-    if (e.which == 88 || e.which == 120) {
+    if (e.which == 88) {
+      isPosing = false;
       $('#intro-music')[0].pause();
       $('#intro-music')[0].load();
-      $('.ryu-posing').hide();
-      $('.ryu-ready').show();
+      $('.ryu-posing, .hulk-ryu-posing').hide();
+      $('.ryu-ready, .hulk-ryu-ready').show();
     }
 });
-
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 1;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
-
-var isMusicPlaying = false;
-function playMusic () {
-  isMusicPlaying = !isMusicPlaying;
-  if (isMusicPlaying) {
-    $('#intro-music')[0].volume = 1;
-    $('#intro-music')[0].play();
-  }
-}
